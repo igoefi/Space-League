fc4d43c87408fb3ad1bbf9ba852eb1a39b2357f7
Prokachka <Utsup110289@gmail.com>
2022-12-24 18:35:04 +0300

    Update

--- Assets/_Scripts/Shoot/Shotgun.cs
+++ Assets/_Scripts/Shoot/Shotgun.cs
@@ -10,8 +10,8 @@ public class Shotgun : Weapons
         GameObject projectile_1 = Instantiate(projectilePrefab, projectileSpauner.transform.position+new Vector3(1,0,0), projectileSpauner.transform.rotation);
         GameObject projectile = Instantiate(projectilePrefab, projectileSpauner.transform.position, projectileSpauner.transform.rotation);
         GameObject projectile_2 = Instantiate(projectilePrefab, projectileSpauner.transform.position - new Vector3(1, 0, 0), projectileSpauner.transform.rotation);
-        projectile_1.transform.rotation = Quaternion.Euler(projectile_1.transform.rotation.eulerAngles.x, projectile_1.transform.rotation.eulerAngles.y + 20, projectile_1.transform.rotation.eulerAngles.z);
-        projectile_2.transform.rotation = Quaternion.Euler(projectile_1.transform.rotation.eulerAngles.x, projectile_1.transform.rotation.eulerAngles.y - 20, projectile_1.transform.rotation.eulerAngles.z);
+        projectile_1.transform.rotation = Quaternion.Euler(projectile.transform.rotation.eulerAngles.x, projectile.transform.rotation.eulerAngles.y + 20, projectile.transform.rotation.eulerAngles.z);
+        projectile_2.transform.rotation = Quaternion.Euler(projectile.transform.rotation.eulerAngles.x, projectile.transform.rotation.eulerAngles.y - 20, projectile.transform.rotation.eulerAngles.z);
 
         if (projectile.TryGetComponent<ProjectileScript>(out ProjectileScript projectileScript))
         {
